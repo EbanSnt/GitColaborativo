d6100713f06cc3359f8e897bd17668f8c7449f32
Esteban Santillan <negociosebansnt@gmail.com>
2024-07-03 20:40:49 -0300

    Primer commit

---
+++ src/test/java/com/codingdojo/esteban/GitColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.esteban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
